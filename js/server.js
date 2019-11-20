c4c1cef62fa5ce58db25402bea5b79322c106040
ghaid01 <ghaid.zahran2001@gmail.com>
2019-11-20 17:40:29 +0200

    port

---
+++ js/server.js
@@ -0,0 +1,10 @@
+'use strict'
+
+require.env(express,cors,dotenv);
+
+var express = require('express');
+var cors = require('cors');
+
+var server = app.listen(3000, function (){
+    console.log('hello');
+})
